bc1e18a93175efab1fcd3f89160fe7567faca7c0
itsdikey <garo.tadevosyan@gmail.com>
2021-08-22 14:58:28 +0400

    added fsm base

---
+++ Assets/__Scripts/FSM/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Demo.FSM
+{
+    [CreateAssetMenu(menuName = "FSM/State")]
+    public sealed class State : BaseState
+    {
+        public List<FSMAction> Action = new List<FSMAction>();
+        public List<Transition> Transitions = new List<Transition>();
+
+        public override void Execute(BaseStateMachine machine)
+        {
+            foreach (var action in Action)
+                action.Execute(machine);
+
+            foreach(var transition in Transitions)
+                transition.Execute(machine);
+        }
+    }
+}
---
+++ Assets/__Scripts/FSM/Transition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Demo.FSM
+{
+    [CreateAssetMenu(menuName = "FSM/Transition")]
+    public sealed class Transition : ScriptableObject
+    {
+        public Decision Decision;
+        public BaseState TrueState;
+        public BaseState FalseState;
+
+        public void Execute(BaseStateMachine stateMachine)
+        {
+            if(Decision.Decide(stateMachine))
+                stateMachine.CurrentState = TrueState;
+            else
+                stateMachine.CurrentState = FalseState;
+        }
+    }
+}
